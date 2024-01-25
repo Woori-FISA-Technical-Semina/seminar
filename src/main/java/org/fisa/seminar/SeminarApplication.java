3ede4a9fd410343fac1c0fdec139e68887e6e762
parfait2 <parkyl0208@naver.com>
2024-01-25 22:38:51 +0900

    [Dev] Redis 설정 적용

--- src/main/java/org/fisa/seminar/SeminarApplication.java
+++ src/main/java/org/fisa/seminar/SeminarApplication.java
@@ -2,7 +2,9 @@ package org.fisa.seminar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class SeminarApplication {
 
